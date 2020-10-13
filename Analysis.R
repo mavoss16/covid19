1cc0dd9d42bfcb2d47becc422b746062fafe99c9
mavoss16 <mavoss16@gmail.com>
2020-10-12 23:02:59 -0500

    10/12 Update

--- Analysis.R
+++ Analysis.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(ggplot2)
 library(magrittr)
 library(leaflet)
-library(tigris)
+library(sf)
 
 
 # Read in data files
@@ -17,7 +17,8 @@ counties = read_rds("ia_counties.rds")
 county_data$Date = as.Date.character(paste(county_data$Year, county_data$Month, county_data$Day, sep = "-"))
 statewide$Date = as.Date.character(paste(statewide$Year, statewide$Month, statewide$Day, sep = "-"))
 
-county_data %<>% left_join(counties %>% mutate(County = NAME) %>% select(County))
+
+counties = left_join(counties %>% mutate(County = NAME), county_data[1:99,])
 
 
 county_test_rate = function(counties = c("Benton", "Story", "Linn", "Polk")){
@@ -47,8 +48,18 @@ rolling_state_test_rate = function(){
     ggplot(aes(x = Date, y = PosTestRate14)) + geom_line()
 }
 
+
 map_county_rolling = function(){
+  scale_range = c(min(counties$PosTestRate14, na.rm = T), max(counties$PosTestRate14, na.rm = T))
+  pal = colorNumeric("Blues", scale_range, na.color = "#aaff56")
   
+  leaflet() %>%
+    addTiles() %>%
+    addPolygons(data = st_transform(counties, crs = '+proj=longlat +datum=WGS84'),
+                weight = 1, color = "#333333", fillColor = pal(counties$PosTestRate14), fillOpacity = 0.9, label = paste0(counties$County, ", ", round(counties$PosTestRate14, 2))) %>%
+    addLegend(position = "topright", pal = pal, values = scale_range, opacity = 0.9,
+              title = "14 day<br>positive<br>test rate")  %>%
+    setView(lng = -93.119723, lat = 42.156431, zoom = 7)
 }
 
 
