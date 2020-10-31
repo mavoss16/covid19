ce8a93d9a0eed7d637a36f3d04e92ec31da0b4be
mavoss16 <mavoss16@gmail.com>
2020-10-30 21:28:23 -0500

    10/30 Update

---
+++ add_fips.R
@@ -0,0 +1,11 @@
+library(readr)
+library(dplyr)
+library(magrittr)
+
+ia_counties = read_rds("county_fips.rds")
+
+county_data = read_csv("County_data.csv")
+
+counties_fips = left_join(county_data, ia_counties %>% select(County, co_fips))
+
+write_csv(counties_fips, "County_data_fips.csv")
